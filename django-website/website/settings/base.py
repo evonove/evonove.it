896b6de9cb74e6cbd4203472fd7c959d4d6ee225
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2016-01-19 16:11:39 +0100

    added settings contrib app

--- django-website/website/settings/base.py
+++ django-website/website/settings/base.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = (
     'wagtail.wagtailsearch',
     'wagtail.wagtailadmin',
     'wagtail.wagtailcore',
+    'wagtail.contrib.settings',
 
     'modelcluster',
     'compressor',
@@ -91,6 +92,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'wagtail.contrib.settings.context_processors.settings',
             ],
         },
     },
