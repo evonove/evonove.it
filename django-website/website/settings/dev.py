c14320ce4d4306c368b5590fbea3a0e4976b0c5d
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2016-01-19 12:01:52 +0100

    providing dev, production and test settings

--- django-website/website/settings/dev.py
+++ django-website/website/settings/dev.py
@@ -1,10 +1,18 @@
 from .base import *
 
 
+# removing security enforcement in development mode
 DEBUG = True
+SECRET_KEY = env('SECRET_KEY', '1234567890')
 
-for template_engine in TEMPLATES:
-    template_engine['OPTIONS']['debug'] = True
-
-SECRET_KEY = 'l5d6!42z#i)s2lhfz9v6i=z2513$a1sd2fn1oakb=(c%jof^j&'
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# enabling console loggers
+LOGGING['loggers'] = {
+    'django': {
+        'handlers': ['console'],
+        'level': env('DJANGO_LOG_LEVEL', 'INFO'),
+    },
+    'website': {
+        'handlers': ['console'],
+        'level': env('WEBSITE_LOG_LEVEL', 'DEBUG'),
+    },
+}
---
+++ django-website/website/settings/production.py
@@ -0,0 +1,32 @@
+from .base import *
+
+
+# security enforcement
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = env('DJANGO_SECURE_SSL_REDIRECT', True)
+SESSION_COOKIE_SECURE = env('DJANGO_SESSION_COOKIE_SECURE', True)
+
+# uncomment for cross-domain cookies
+# SESSION_COOKIE_DOMAIN = ".{}".format(env("DJANGO_ALLOWED_HOSTS"))
+
+# emails
+DEFAULT_EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_BACKEND = env('DJANGO_EMAIL_BACKEND', EMAIL_BACKEND_DEFAULT)
+EMAIL_HOST = env("EMAIL_HOST")
+EMAIL_PORT = env("EMAIL_HOST_PORT")
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+EMAIL_USE_TLS = True
+
+# logging
+LOGGING['loggers'] = {
+    'django': {
+        'handlers': ['console', 'syslog'],
+        'level': env('DJANGO_LOG_LEVEL', 'INFO'),
+    },
+    'website': {
+        'handlers': ['logstash', 'syslog'],
+        'level': env('WEBSITE_LOG_LEVEL', 'INFO'),
+    },
+}
