c86b93dabe0a3d937da25d77649d87dcf4eba620
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2016-01-21 10:21:35 +0100

    removing old blog index and blog page implementation; providing a new BlogPage model

--- django-website/blog/models.py
+++ django-website/blog/models.py
@@ -1,40 +1,43 @@
 from django.db import models
+from django.utils.translation import ugettext as _
 
-from wagtail.wagtailcore.models import Page
-from wagtail.wagtailcore.fields import RichTextField
-from wagtail.wagtailadmin.edit_handlers import FieldPanel
-from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
+from modelcluster.fields import ParentalKey
+from modelcluster.contrib.taggit import ClusterTaggableManager
+
+from taggit.models import TaggedItemBase
+
 from wagtail.wagtailsearch import index
+from wagtail.wagtailcore.models import Page
+from wagtail.wagtailcore.fields import StreamField
+from wagtail.wagtailadmin.edit_handlers import FieldPanel, StreamFieldPanel
+from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
+
+from .fields import BlogPageStreamBlock
 
 
-class BlogIndexPage(Page):
-    intro = RichTextField(blank=True)
-
-    content_panels = Page.content_panels + [
-        FieldPanel('intro', classname="full")
-    ]
+class BlogPageTag(TaggedItemBase):
+    content_object = ParentalKey('BlogPage', related_name='tagged_items')
 
 
 class BlogPage(Page):
-    main_image = models.ForeignKey(
+    cover = models.ForeignKey(
         'wagtailimages.Image',
         null=True,
         blank=True,
         on_delete=models.SET_NULL,
         related_name='+'
     )
-    date = models.DateField("Post date")
-    intro = models.CharField(max_length=250)
-    body = RichTextField(blank=True)
+    body = StreamField(BlogPageStreamBlock())
+    tags = ClusterTaggableManager(through=BlogPageTag, blank=True)
+    date = models.DateField(_('Post date'))
 
     search_fields = Page.search_fields + (
-        index.SearchField('intro'),
         index.SearchField('body'),
     )
 
     content_panels = Page.content_panels + [
+        ImageChooserPanel('cover'),
+        FieldPanel('tags'),
+        StreamFieldPanel('body'),
         FieldPanel('date'),
-        ImageChooserPanel('main_image'),
-        FieldPanel('intro'),
-        FieldPanel('body'),
     ]
