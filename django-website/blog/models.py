f9db8720d8aa1371d1c505b46d3bfca4026aa8e6
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2016-01-21 11:29:25 +0100

    providing a blog index page model with the proper get_context method

--- django-website/blog/models.py
+++ django-website/blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.utils.translation import ugettext as _
 
 from modelcluster.fields import ParentalKey
@@ -13,6 +14,47 @@ from wagtail.wagtailadmin.edit_handlers import FieldPanel, StreamFieldPanel
 from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
 
 from .fields import BlogPageStreamBlock
+
+
+class BlogIndexPage(Page):
+    @property
+    def articles(self):
+        """
+        Returns a queryset of live blog articles, ordered from the most recent
+        """
+        articles = BlogPage.objects.live().descendant_of(self)
+        articles = articles.order_by('-date')
+
+        return articles
+
+    def get_context(self, request):
+        """
+        Populates the context with a paginated articles list.
+        They are filtered using the querystring; available filters are:
+            - tag: fetch articles for the chosen tag
+            - page: fetch a list of articles for the selected page number
+        """
+        articles = self.articles
+
+        # Filtering by tag
+        tag = request.GET.get('tag')
+        if tag:
+            articles = articles.filter(tags__name=tag)
+
+        # Pagination
+        page = request.GET.get('page')
+        paginator = Paginator(articles, 10)
+        try:
+            articles = paginator.page(page)
+        except PageNotAnInteger:
+            articles = paginator.page(1)
+        except EmptyPage:
+            articles = paginator.page(paginator.num_pages)
+
+        # Updating the template context
+        context = super().get_context(request)
+        context['articles'] = articles
+        return context
 
 
 class BlogPageTag(TaggedItemBase):
