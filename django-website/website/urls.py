bae03d92a8dbcc4a4b7d7dcaba934fea50ff5cc3
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2016-01-21 22:38:36 +0100

    WhiteNoise serves Django statics and users media files

--- django-website/website/urls.py
+++ django-website/website/urls.py
@@ -13,12 +13,3 @@ urlpatterns = [
     url(r'^sitemap\.xml$', sitemap),
     url(r'', include(wagtail_urls)),
 ]
-
-
-if settings.DEBUG:
-    from django.conf.urls.static import static
-    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-
-    # Serve static and media files from development server
-    urlpatterns += staticfiles_urlpatterns()
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- django-website/website/wsgi.py
+++ django-website/website/wsgi.py
@@ -1,7 +1,15 @@
+from django.conf import settings
 from django.core.wsgi import get_wsgi_application
 
+from whitenoise import WhiteNoise
 from whitenoise.django import DjangoWhiteNoise
 
 
-application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+# WhiteNoise is used to serve Django statics and
+# uploaded media files. In the first case we can improve
+# the server performance using a CDN such as CloudFlare
+application = WhiteNoise(
+    DjangoWhiteNoise(get_wsgi_application()),
+    root=settings.MEDIA_ROOT,
+    prefix='/media/',
+)
