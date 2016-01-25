9a7f313668e506e9a2598a5a7ebd4c432fc6ec1c
Alessio Rapini <alessio.rapini@yahoo.it>
2016-01-25 18:15:45 +0100

    replaced the background image with a content image for the developers and improved People section styles

--- django-website/wheelie/client/js/people.js
+++ django-website/wheelie/client/js/people.js
@@ -1,12 +1,25 @@
 $(document).ready(function() {
 
+    var mobile;
+    if ($(window).width() < 950) {
+        mobile = 1;
+    }
+
     var person = $('.people-container-person');
     var description = $('.people-container-person-description');
 
     var personWidth = person.width();
 
-    description.css({
-        'left': personWidth * 1.618
-    });
+    if (!mobile) {
+        description.css({
+            'left': personWidth * 1.5
+        });
+    }
 
+    if (mobile) {
+        description.click(function() {
+            $(this).toggleClass('fade-in');
+            description.not(this).removeClass('fade-in');
+        });
+    }
 });
