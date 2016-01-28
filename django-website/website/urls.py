02ab6061d2305f7676244094ded3e5643d48f47c
Alessio Rapini <alessio.rapini@yahoo.it>
2016-01-28 16:10:08 +0100

    showing 404 and 500 pages on DEBUG mode

--- django-website/website/urls.py
+++ django-website/website/urls.py
@@ -1,12 +1,23 @@
+from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
+
+from django.views.generic import TemplateView
 
 from wagtail.contrib.wagtailsitemaps.views import sitemap
 from wagtail.wagtailadmin import urls as wagtailadmin_urls
 from wagtail.wagtailcore import urls as wagtail_urls
 
 
-urlpatterns = [
+urlpatterns = []
+
+if settings.DEBUG:
+    urlpatterns += [
+        url(r'^404/$', TemplateView.as_view(template_name='404.html')),
+        url(r'^500/$', TemplateView.as_view(template_name='500.html')),
+    ]
+
+urlpatterns += [
     url(r'^django-admin/', include(admin.site.urls)),
     url(r'^admin/', include(wagtailadmin_urls)),
     url(r'^sitemap\.xml$', sitemap),
