37280e11eae210de6dc6a15ceb156f4bfe074c48
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2016-02-04 16:48:22 +0100

    removing the create blog migration in favor of a create_blog_page django command

---
+++ django-website/blog/management/commands/create_blog_page.py
@@ -0,0 +1,28 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.contrib.contenttypes.models import ContentType
+
+from home.models import HomePage
+from blog.models import BlogIndexPage
+
+
+class Command(BaseCommand):
+    help = 'Creates the initial blog page to the root homepage'
+
+    def handle(self, *args, **options):
+        try:
+            homepage = HomePage.objects.get(slug='home')
+        except HomePage.DoesNotExist:
+            raise CommandError('The Homepage does not exist. Be sure to run the first migration.')
+
+        # creating the blog page instance
+        blog_content_type, created = ContentType.objects.get_or_create(
+                model='blogindexpage', app_label='blog')
+
+        blog = BlogIndexPage(
+            title="Blog",
+            slug='blog',
+            content_type=blog_content_type,
+        )
+
+        # linking the blog page with the Homepage
+        homepage.add_child(instance=blog)
