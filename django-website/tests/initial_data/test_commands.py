455aa9bb2007d7d99c1b8b8241ab9a13a1695f6e
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2016-02-09 17:12:51 +0100

    adding load_test_data test

--- django-website/tests/initial_data/test_commands.py
+++ django-website/tests/initial_data/test_commands.py
@@ -2,9 +2,13 @@ import pytest
 
 from django.core.management import call_command
 
-from wagtail.wagtailcore.models import Page
+from wagtail.wagtailcore.models import Page, Site
+from wagtail.wagtailimages.models import Image
 
-from home.models import HomePage
+from options.models import WebsiteSettings
+
+from home.models import HomePage, Service, Work, TeamMember
+
 from blog.models import BlogPage
 
 
@@ -28,3 +32,35 @@ class TestInitialLoad:
         blog = Page.objects.get(slug='blog')
         homepage = HomePage.objects.get(slug='home')
         assert blog in homepage.get_children()
+
+    def test_load_test_data(self):
+        """
+        Alice is a developer that wants to update JavaScript and CSS code but unforunately
+        she blames the absence of initial data. Indeed, many styles are available only if
+        services, works, and the team is loaded. For this reason, she launches the
+        'load_test_data' command.
+        """
+        # a real Dunder Muffline company is created!
+        call_command('load_test_data')
+
+        # grabbing default settings
+        homepage = HomePage.objects.get(slug="home")
+        current_site = Site.objects.get(root_page=homepage)
+        website_settings = WebsiteSettings.for_site(current_site)
+
+        # asserting that a real company is available
+        assert website_settings.name is not None
+        assert website_settings.email is not None
+        assert website_settings.phone is not None
+        assert website_settings.vat is not None
+        assert website_settings.github is not None
+        assert website_settings.twitter is not None
+        assert website_settings.facebook is not None
+
+        # placeholder images should be available
+        assert Image.objects.count() == 2
+
+        # and so services, works and team members
+        assert Service.objects.count() == 3
+        assert Work.objects.count() == 3
+        assert TeamMember.objects.count() == 10
