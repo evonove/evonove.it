334511f2e1877d4de915bb5ff573072943291466
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2016-02-09 17:59:06 +0100

    refactoring the loading command

--- django-website/home/management/commands/load_test_data.py
+++ django-website/home/management/commands/load_test_data.py
@@ -47,177 +47,78 @@ class Command(BaseCommand):
         website_settings.facebook = "http://www.facebook.com/evonovesrl"
         website_settings.save()
 
+        # put here your data
+        placeholders = [
+            {"title": "works", "file": work_placeholder, "w": 725, "h": 725},
+            {"title": "team", "file": team_placeholder, "w": 483, "h": 483},
+        ]
+
+        services = [
+            {"slogan": "A Paper Company", "technologies": "We are the best Paper Company"},
+            {"slogan": "Best place where to work", "technologies": "We have the world's best boss"},
+            {"slogan": "Based on Scranton, Pennsylvania", "technologies": "We ship everywhere!"},
+        ]
+
+        works = [
+            {"name": "Selling stuff"},
+            {"name": "Merging branches"},
+            {"name": "Boat parties"},
+        ]
+
+        team = [
+            {"firstname": "Ryan", "lastname": "Howard", "role": "Temporary Worker"},
+            {"firstname": "Toby", "lastname": "Flenderson", "role": "Human Resources Representative"},
+            {"firstname": "Meredith", "lastname": "Palmer", "role": "Supplier Relations Representative"},
+            {"firstname": "Kevin", "lastname": "Malone", "role": "Accountant"},
+            {"firstname": "Angela", "lastname": "Martin", "role": "Senior Accountant"},
+            {"firstname": "Pam", "lastname": "Beesly", "role": "Receptionist"},
+            {"firstname": "Stanley", "lastname": "Hudson", "role": "Sales Representative"},
+            {"firstname": "Jim", "lastname": "Halpert", "role": "Sales Representative"},
+            {"firstname": "Dwight", "lastname": "Schrute", "role": "Assistant to the Regional Manager"},
+            {"firstname": "Michael", "lastname": "Scott", "role": "Best boss, Regional Manager"},
+        ]
+
         # add a placeholder image for Works section
-        work_image, created = Image.objects.get_or_create(
-            title="Placeholder",
-            width=725,
-            height=725,
-        )
+        images = {}
+        for placeholder in placeholders:
+            image, created = Image.objects.get_or_create(
+                title=placeholder["title"],
+                width=placeholder["w"],
+                height=placeholder["h"],
+            )
 
-        if created:
-            work_image.file.save("works-placeholder.png", work_placeholder, save=True)
+            if created:
+                image.file.save(placeholder["title"], placeholder["file"], save=True)
 
-        # add a placeholder image for Team section
-        team_image, created = Image.objects.get_or_create(
-            title="Placeholder team",
-            width=483,
-            height=483,
-        )
-
-        if created:
-            team_image.file.save("man-silhouette.png", team_placeholder, save=True)
+            images[placeholder["title"]] = image
 
         # add some services
-        Service.objects.get_or_create(
-            slogan="A Paper Company",
-            technologies="We are the best Paper Company",
-            description=lorem,
-            page=homepage,
-        )
-
-        Service.objects.get_or_create(
-            slogan="Best place where to work",
-            technologies="We have the world's best boss",
-            description=lorem,
-            page=homepage,
-        )
-
-        Service.objects.get_or_create(
-            slogan="Based on Scranton, Pennsylvania",
-            technologies="We ship everywhere!",
-            description=lorem,
-            page=homepage,
-        )
+        for service in services:
+            Service.objects.get_or_create(
+                slogan=service["slogan"],
+                technologies=service["technologies"],
+                description=lorem,
+                page=homepage,
+            )
 
         # add some works
-        Work.objects.get_or_create(
-            name="Selling stuff",
-            description=lorem,
-            cover=work_image,
-            page=homepage,
-        )
-
-        Work.objects.get_or_create(
-            name="Merging branches",
-            description=lorem,
-            cover=work_image,
-            page=homepage,
-        )
-
-        Work.objects.get_or_create(
-            name="Boat parties",
-            description=lorem,
-            cover=work_image,
-            page=homepage,
-        )
+        for work in works:
+            Work.objects.get_or_create(
+                name=work["name"],
+                description=lorem,
+                cover=images["works"],
+                page=homepage,
+            )
 
         # add some team members
-        TeamMember.objects.get_or_create(
-            firstname="Ryan",
-            lastname="Howard",
-            role="Temporary Worker",
-            bio=lorem,
-            photo=team_image,
-            website="https://evonove.it",
-            github="https://github.com/evonove/",
-            page=homepage,
-        )
-
-        TeamMember.objects.get_or_create(
-            firstname="Toby",
-            lastname="Flenderson",
-            role="Human Resources Representative",
-            bio=lorem,
-            photo=team_image,
-            website="https://evonove.it",
-            github="https://github.com/evonove/",
-            page=homepage,
-        )
-
-        TeamMember.objects.get_or_create(
-            firstname="Meredith",
-            lastname="Palmer",
-            role="Supplier Relations Representative",
-            bio=lorem,
-            photo=team_image,
-            website="https://evonove.it",
-            github="https://github.com/evonove/",
-            page=homepage,
-        )
-
-        TeamMember.objects.get_or_create(
-            firstname="Kevin",
-            lastname="Malone",
-            role="Accountant",
-            bio=lorem,
-            photo=team_image,
-            website="https://evonove.it",
-            github="https://github.com/evonove/",
-            page=homepage,
-        )
-
-        TeamMember.objects.get_or_create(
-            firstname="Angela",
-            lastname="Martin",
-            role="Senior Accountant",
-            bio=lorem,
-            photo=team_image,
-            website="https://evonove.it",
-            github="https://github.com/evonove/",
-            page=homepage,
-        )
-
-        TeamMember.objects.get_or_create(
-            firstname="Pam",
-            lastname="Beesly",
-            role="Receptionist",
-            bio=lorem,
-            photo=team_image,
-            website="https://evonove.it",
-            github="https://github.com/evonove/",
-            page=homepage,
-        )
-
-        TeamMember.objects.get_or_create(
-            firstname="Stanley",
-            lastname="Hudson",
-            role="Sales Representative",
-            bio=lorem,
-            photo=team_image,
-            website="https://evonove.it",
-            github="https://github.com/evonove/",
-            page=homepage,
-        )
-
-        TeamMember.objects.get_or_create(
-            firstname="Jim",
-            lastname="Halpert",
-            role="Sales Representative",
-            bio=lorem,
-            photo=team_image,
-            website="https://evonove.it",
-            github="https://github.com/evonove/",
-            page=homepage,
-        )
-
-        TeamMember.objects.get_or_create(
-            firstname="Dwight",
-            lastname="Schrute",
-            role="Assistant to the Regional Manager",
-            bio=lorem,
-            photo=team_image,
-            website="https://evonove.it",
-            github="https://github.com/evonove/",
-            page=homepage,
-        )
-
-        TeamMember.objects.get_or_create(
-            firstname="Michael",
-            lastname="Scott",
-            role="Best boss, Regional Manager",
-            bio=lorem,
-            photo=team_image,
-            website="https://evonove.it",
-            github="https://github.com/evonove/",
-            page=homepage,
-        )
+        for employee in team:
+            TeamMember.objects.get_or_create(
+                firstname=employee["firstname"],
+                lastname=employee["lastname"],
+                role=employee["role"],
+                bio=lorem,
+                photo=images["team"],
+                website="https://evonove.it",
+                github="https://github.com/evonove/",
+                page=homepage,
+            )
