2162b2d8a94fc2fc8198e09ca0690ebcc3efd5d3
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2016-02-09 12:36:58 +0100

    test improvement

--- django-website/tests/initial_data/test_commands.py
+++ django-website/tests/initial_data/test_commands.py
@@ -2,6 +2,9 @@ import pytest
 
 from django.core.management import call_command
 
+from wagtail.wagtailcore.models import Page
+
+from home.models import HomePage
 from blog.models import BlogPage
 
 
@@ -14,7 +17,14 @@ class TestInitialLoad:
         page. For this reason, she launches the 'create_blog_page' command.
             - The blog page should be created as a child of the site homepage
         """
+        # the blog page is not created through migrations
         assert BlogPage.objects.count() == 0
 
+        # the blog page is created
         call_command('create_blog_page')
         assert BlogPage.objects.count() == 1
+
+        # and is child of the homepage
+        blog = Page.objects.get(slug='blog')
+        homepage = HomePage.objects.get(slug='home')
+        assert blog in homepage.get_children()
