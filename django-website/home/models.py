a1b3e8b467398cdc7f909164812df3dc035a903e
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2016-05-11 10:46:29 +0200

    fixed EW-79: forcing a max length for developers' bio

--- django-website/home/models.py
+++ django-website/home/models.py
@@ -64,7 +64,7 @@ class TeamMember(Orderable):
     firstname = models.CharField(max_length=100)
     lastname = models.CharField(max_length=100)
     role = models.CharField(max_length=150, help_text=_('Team member company role'))
-    bio = RichTextField(max_length=1000, help_text=_('The team member bio'))
+    bio = RichTextField(max_length=320, help_text=_('The team member bio'))
     gravatar = models.CharField(max_length=150, help_text=_('Add your Gravatar email'), null=True, blank=True)
     photo = models.ForeignKey(Image, null=True, blank=True, related_name='+')
 
