28307ab8e450f606cdb384003eb80c32d8b60810
Alessio Rapini <alessio.rapini@yahoo.it>
2016-07-22 15:37:57 +0200

    add a slide effect to show the tag list on mobile

--- django-website/wheelie/client/js/blog-tags.js
+++ django-website/wheelie/client/js/blog-tags.js
@@ -1,20 +1,55 @@
 (function() {
 
-    var sidebar = document.querySelector('.sidebar');
-    var tags = document.querySelector('.sidebar-tags');
-
-    if(!sidebar || !tags) {
-        return;
+    var mobile;
+    if ($(window).width() < 950) {
+        mobile = true;
     }
+
+    var sidebar = document.querySelector('.sidebar');
+    var tags = document.querySelector('.sidebar-box');
+    var sidebarButton = document.querySelector('.sidebar-box-header');
+    var tagList = document.querySelector('.sidebar-box-taglist');
+    var arrow = document.querySelector('.sidebar-box-header-button');
+    var tagSelected = document.querySelector('.sidebar-box-taglist li a.is-active');
 
     var sidebarTop = $(sidebar).offset().top;
 
-    $(window).scroll(function() {
-        if($(window).scrollTop() > sidebarTop) {
+    if (!sidebar) {
+        return;
+    }
+
+    if (!mobile) {
+        $(window).scroll(fixTagList);
+    } else {
+        $(sidebarButton).click(toggleTagList);
+
+        // If a tag is selected, tag list remains open when user selects other tags.
+        if (tagSelected) {
+            tagListOpenAtRefresh();
+        }
+    }
+
+    function fixTagList() {
+        if ( $(window).scrollTop() > sidebarTop ) {
             $(tags).addClass('is-fixed');
         } else {
             $(tags).removeClass('is-fixed');
         }
-    });
+    }
 
+    function toggleTagList() {
+        $(tagList).toggleClass('is-shown');
+        $(arrow).toggleClass('is-shown');
+    }
+
+    function tagListOpenAtRefresh() {
+        $(tagList).toggleClass('is-shown').toggleClass('is-open');
+        $(arrow).toggleClass('is-shown').toggleClass('is-open');
+        $(sidebarButton).click(removeTagListTransition);
+    }
+
+    function removeTagListTransition() {
+        $(tagList).removeClass('is-open');
+        $(arrow).removeClass('is-open');
+    }
 })();
