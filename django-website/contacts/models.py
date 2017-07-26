2bee0c0d24a10c0390f59ad0197a6709bd553524
Alessandro De Angelis <alessandrodea22@gmail.com>
2017-07-26 12:42:54 +0200

    EW-176: Contacts page

--- django-website/contacts/models.py
+++ django-website/contacts/models.py
@@ -1,5 +1,13 @@
+from django.db import models
+from django.utils.translation import ugettext as _
+
 from wagtail.wagtailcore.models import Page
+from wagtail.wagtailadmin.edit_handlers import FieldPanel
 
 
 class ContactsPage(Page):
-    pass
+    job_email = models.EmailField(max_length=255, null=True, blank=True, help_text=_('job email address'))
+
+    content_panels = Page.content_panels + [
+        FieldPanel('job_email'),
+    ]
