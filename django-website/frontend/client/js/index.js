eac3eb0a383d68a39b13194d860bd1001c978f46
Alessio Rapini <alessio.rapini@yahoo.it>
2017-08-10 13:14:13 +0200

    Create transitions for subtitles

--- django-website/frontend/client/js/index.js
+++ django-website/frontend/client/js/index.js
@@ -1,5 +1,9 @@
 // Import Sass files
 require('../scss/main.scss');
 
-// Import JavaScript files
+// Import JavaScript vendor files
+require('../../node_modules/waypoints/lib/noframework.waypoints.js');
+
+// Import JavaScript custom files
 require('./scripts/nav.js');
+require('./scripts/reveal-subtitles.js');
---
+++ django-website/frontend/client/js/scripts/reveal-subtitles.js
@@ -0,0 +1,33 @@
+// Reveal subtitles during scroll
+
+const payoff = document.querySelectorAll('.payoff > .rich-text > p');
+const subtitles = document.querySelectorAll('.js-show_subtitle h2');
+
+/**
+ * Use Waypoint to reveal every subtitles when they are on the viewport
+ */
+function revealSubtitle() {
+  for (let i = 0; i < subtitles.length; i += 1) {
+    this.element.classList.add('is-subtitle_shown');
+    this.destroy();
+  }
+}
+
+/**
+ * Reveal the payoff when the homepage is loaded
+ */
+function revealPayoff() {
+  for (let i = 0; i < payoff.length; i += 1) {
+    payoff[i].classList.add('is-subtitle_shown');
+  }
+}
+
+for (let i = 0; i < subtitles.length; i += 1) {
+  new Waypoint({
+    element: subtitles[i],
+    handler: revealSubtitle,
+    offset: '76.999%',
+  });
+}
+
+window.addEventListener('load', revealPayoff);
